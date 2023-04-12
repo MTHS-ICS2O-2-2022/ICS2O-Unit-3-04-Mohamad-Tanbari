a207e77e64265acb29f64033021fe11c8942b695
Mohamad-Tanbari <124389317+Mohamad-Tanbari@users.noreply.github.com>
2023-04-12 17:55:10 +0000

    fixed misspelling

--- js/script.js
+++ js/script.js
@@ -4,19 +4,22 @@
 // Created on: Sep 2023
 // This file contains the JS functions for index.html
 
-'use strict'
+"use strict"
 /*
-* This function 
-*/
-function calculate () {
+ * This function
+ */
+function calculate() {
   // This function converts fahrenheit to celsius
 
   // Recieve input from user
-  const fahrenheitTemperature = parseFloat(document.getElementById('farenheit-temperature').value)
+  const fahrenheitTemperature = parseFloat(
+    document.getElementById("fahrenheit-temperature").value
+  )
 
   // Calculate volume
-  const celsius = (fahrenheitTemperature - 32) * .5556 
+  const celsius = (fahrenheitTemperature - 32) * 0.5556
 
   // Output volume to the user
-  document.getElementById('answer').innerHTML = "Temperature in celsius: " + celsius.toFixed(2) + " °C"
+  document.getElementById("answer").innerHTML =
+    "Temperature in celsius: " + celsius.toFixed(2) + " °C"
 }
