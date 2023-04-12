4d4535d01cf3d68a8903493c214e89372d005635
Mohamad-Tanbari <124389317+Mohamad-Tanbari@users.noreply.github.com>
2023-04-12 16:54:59 +0000

    built website

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 Mohamad All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Mohamad
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+/*
+* This function 
+*/
+function calculate () {
+  // This function calculates the volume of a sphere
+
+  // Recieve input from user
+  const farenheitTemperature = parseFloat(document.getElementById('farenheit-temperature').value)
+
+  // Calculate volume
+  const celceius = (farenheitTemperature - 32) * .5556 
+
+  // Output volume to the user
+  document.getElementById('answer').innerHTML = "Temperature in Celceius: " + celceius.toFixed(2) + " °C"
+}
